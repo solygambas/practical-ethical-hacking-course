fe5f78bf450d1d9f711050c7837841c5238ee8dd
solygambas <51904909+solygambas@users.noreply.github.com>
2024-03-26 11:40:00 +0100

    feat: add python basics

---
+++ 07-intro-to-python/first.py
@@ -0,0 +1,69 @@
+# no need to be a dev to be successful, being able to read code
+
+# STRINGS
+
+# linux
+##!/bin/python3
+
+# # Print Hello, World! to the console
+# print("Hello, World!")
+
+# # Triple quote for multi-line strings
+# print("""Hello, 
+# World!""")
+
+# # Concatenation
+# print("Hello, " + "World!")
+
+# # Print a new line
+# print("Hello, \nWorld!")
+
+# MATH
+
+# # Addition
+# print(5 + 5)
+# # Subtraction
+# print(5 - 5)
+# # Multiplication
+# print(5 * 5)
+# # Division
+# print(5 / 5) # 1.0 float
+# # Exponent
+# print(5 ** 5)
+# # Modulus
+# print(50 % 6) # 2 (remainder)
+# # Floor division
+# print(50 // 6) # 8 (quotient)
+
+# VARIABLES & METHODS
+
+# Variables
+quote = "All is fair in love and war."
+print(quote)
+# Methods
+print(quote.upper()) # ALL IS FAIR IN LOVE AND WAR.
+print(quote.lower()) # all is fair in love and war.
+print(quote.title()) # All Is Fair In Love And War.
+print(len(quote)) # 28
+name = "Heath"
+age = 30
+gpas = 3.7 # float
+print(int(age)) # 30
+print(int(gpas)) # 3
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+
+age += 1
+print(age) # 31
+
+birthday = 1
+age += birthday
+print(age) # 32
+
+# FUNCTIONS
+
+
+
+
+
+
